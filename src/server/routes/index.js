3cdddcaedc860a3638672c89dda339b1f711d562
DBuckley0126 <DannyBoy.0126>
2020-04-29 13:11:24 +0100

    Seperate API response functionality to controllers

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,6 +1,11 @@
 const Router = require("koa-router");
 
+const indexControllers = require("../controllers/index");
+
 const router = new Router();
+
+
+router.get("/", indexControllers.index);
 
 router.get("/", async ctx => {
   ctx.body = {
